e7294b16e62aa984a0a1bcf77ca858821a3795ba
nmparfyonov <nikitamaximovi4@gmail.com>
2024-10-17 23:03:42 +0300

    feat: public, private, bastion security groups

--- security_groups.tf
+++ security_groups.tf
@@ -1,5 +1,7 @@
-resource "aws_security_group" "k3s" {
-  vpc_id = aws_vpc.main_vpc.id
+resource "aws_security_group" "k3s_sg" {
+  name        = "k3s-security-group"
+  vpc_id      = aws_vpc.main_vpc.id
+  description = "Security group for the k3s host"
 
   ingress {
     from_port   = 6443
@@ -13,5 +15,99 @@ resource "aws_security_group" "k3s" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-security-group"
+  description = "Security group for the bastion host"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "Allow SSH from your IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_PUBLIC_IP/32"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Bastion Security Group"
+  }
+}
+
+resource "aws_security_group" "public_sg" {
+  name        = "public-security-group"
+  description = "Security group for public subnet"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Public Security Group"
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "private-security-group"
+  description = "Security group for private subnet"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description     = "Allow inbound traffic from public subnets"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Private Security Group"
   }
 }
