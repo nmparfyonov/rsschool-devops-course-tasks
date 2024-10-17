a7f58f111a62062ad73ba444d196444746464101
nmparfyonov <nikitamaximovi4@gmail.com>
2024-10-18 00:13:21 +0300

    fix: ec2 security group definition

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion" {
   subnet_id                   = aws_subnet.public_subnet[0].id
   associate_public_ip_address = true
   key_name                    = var.ssh_key_name
-  security_groups             = [aws_security_group.public_sg.id]
+  vpc_security_group_ids      = [aws_security_group.public_sg.id]
   tags = {
     Name = "Bastion Host"
   }
