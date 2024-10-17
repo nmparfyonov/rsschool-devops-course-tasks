40a3980f4f54d52f81afc0b2bb1aab3ccbd81feb
nmparfyonov <nikitamaximovi4@gmail.com>
2024-10-17 22:25:38 +0300

    feat: security group

---
+++ security_groups.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "k3s" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
